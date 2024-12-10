f05ae6d2f0e8911651a7c899e053c7b5626b7d67
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-12-10 16:29:11 -0700

    Starting on thinking in 3d

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './assets/style.css';
+import './assets/twoDimensionStyle.css';
+import './assets/thinkingThreeDimensionStyle.css';
 
 import React from 'react';
 import TwoDimensionalGrid from './Components/TwoDimensionalGrid';
-
+import ThinkingThreeDimensions from './Components/ThinkingThreeDimensions';
+import ThreeDimensionalGrid from './Components/ThreeDimensionalGrid';
 
 function App() {
 
   return (
     <>
       <TwoDimensionalGrid />
+      <ThinkingThreeDimensions />
+      {/* <ThreeDimensionalGrid /> */}
     </>
   )
 }
